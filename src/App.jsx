bad19bba1d12a733821939ec90f039f6ffddbe29
Prajwal <prajwalnaik159@gmail.com>
2025-07-22 23:51:41 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,60 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
+  const [countryOptions, setCountryOptions] = useState([])
 
   useEffect(() => {
     console.log(`a`)
     const fetchOpenHolidays = async () => {
-      const response = await axios.get('https://openholidaysapi.org/Countries')
-      //const data = await response.json()
-      console.log(response.data)
+      try {
+        const res = await axios.get('https://openholidaysapi.org/Countries')
+        //const data = await response.json()
+        console.log(res.data)
+
+        const allCountyName = []
+
+        res.data.forEach((country) => {
+          country.name.forEach((item) => {
+            allCountyName.push({
+              text: item.text,
+              language: item.language
+            });
+          });
+        });
+        console.log(allCountyName)
+
+        const unique = Array.from(
+          new Map(
+            allCountyName.map(item => {
+              return [
+                `${item.text}`, item,
+              ]
+            })
+          ).values()
+        ).sort((a, b) => a.text.localeCompare(b.text))
+        setCountryOptions(unique)
+
+      }
+      catch (error) {
+        console.error("Error fetching countries:", error);
+      }
     }
     fetchOpenHolidays();
   }, [])
+  console.log(`countryOptions`, countryOptions)
 
 
   return (
     <>
-
+      <h2>Country Selector</h2>
+      <select name="select country">
+        {
+          countryOptions.map((option, index) => (
+            <option key={index} value={option.text}>{option.text}</option>
+          )
+        )
+        }
+      </select>
     </>
   )
 }
